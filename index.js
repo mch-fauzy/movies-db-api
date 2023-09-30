b3074be4b9fd87d00f380a0d6a7e6f384dde1312
mucha-fauzy <mucha.fauzy@gmail.com>
2023-09-30 16:32:13 +0700

    feat: edit index.js

--- index.js
+++ index.js
@@ -17,22 +17,16 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-app.get('/home', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.status(200).json('Welcome, your app is working well');
-})
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }))
 
 app.use('/v1', UserRouter);
 app.use('/v1', MovieRouter);
 
-// Error handling middleware
-app.use((err, res) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
+app.get('/home', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.status(200).json('Welcome, your app is working well');
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
