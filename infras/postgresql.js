72114b9a34bd060c813f275c91ce4ac6ea26a4e6
mucha-fauzy <mucha.fauzy@gmail.com>
2023-09-30 16:12:35 +0700

    feat: add config for vercel postgres

--- infras/postgresql.js
+++ infras/postgresql.js
@@ -4,11 +4,7 @@ dotenv.config();
 
 // Edit the config in .env
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
