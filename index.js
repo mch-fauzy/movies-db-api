5f2c44388194b14b3ecae326725743b0137d3ed5
mucha-fauzy <mucha.fauzy@gmail.com>
2023-09-30 16:47:22 +0700

    feat: edit index.js

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }))
-
 app.use('/v1', UserRouter);
 app.use('/v1', MovieRouter);
 
-app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.status(200).json('Welcome, your app is working well');
-})
+// app.get('/', (req, res) => {
+//   res.setHeader('Content-Type', 'text/html');
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//   res.status(200).json('Welcome, your app is working well');
+// })
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
