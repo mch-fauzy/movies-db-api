f491bbd9307afe76c70f018c5ed00f3d90600bcd
mucha-fauzy <mucha.fauzy@gmail.com>
2023-10-02 13:43:33 +0700

    feat: get process env from config

--- infras/postgresql.js
+++ infras/postgresql.js
@@ -1,14 +1,13 @@
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
-dotenv.config();
+const CONFIG = require('../configs');
 
 // Edit the config in .env
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: CONFIG.POSTGRES.HOST,
+  port: CONFIG.POSTGRES.PORT,
+  database: CONFIG.POSTGRES.NAME,
+  user: CONFIG.POSTGRES.USER,
+  password: CONFIG.POSTGRES.PASSWORD,
 });
 
 module.exports = pool;
