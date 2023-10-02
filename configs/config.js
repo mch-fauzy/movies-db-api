ddcde1d31e7c0f6482c226e86851a916563e58dc
mucha-fauzy <mucha.fauzy@gmail.com>
2023-10-02 14:40:23 +0700

    feat: add configs

---
+++ configs/config.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const { logger } = require('../utils');
+const fs = require('fs');
+
+const Environtment = () => {
+    try {
+        if (fs.existsSync('.env')) {
+            dotenv.config({ path: '.env' });
+            logger.info('[Environtment] Loaded default environment: .env');
+        } else {
+            logger.warn('[Environtment] Default .env file not found');
+        }
+    } catch {
+        logger.error("[Environtment] Error loading environment variables");
+        process.exit(1);
+    }
+}
+
+Environtment();
+
+module.exports = {
+    APP: {
+        PORT: process.env.APP_PORT,
+    },
+    VERCEL_POSTGRES: {
+        URL: process.env.POSTGRES_URL
+    },
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
+};
